466c7f0be06ec0a10155d2b95d2408384f11d645
Jaiabhisshek8 <jaiabhissheks@gmail.com>
2023-12-26 15:59:08 +0530

    Mission accomplished

---
+++ Counter/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  reset = () => {
+    this.setState({ count: 0 });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="card">
+          <h1>Counter App</h1>
+          <div>
+            <h2>{this.state.count}</h2>
+            <button onClick={this.increment} className='green'>Increment</button>
+            <button onClick={this.decrement} className='red'>Decrement</button>
+            <button onClick={this.reset} className='reset'>Reset</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
